09835fdc774a5feffa08ae10059395070e809520
Papekhaly <papekhaly96@outlook.fr>
2018-02-25 22:07:09 +0100

    Update restauration.sh

--- restauration.sh
+++ restauration.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 
 
-echo "CREATE DATABASE test" | mysql -u khaly -p 
+echo "CREATE DATABASE baseArestaurer" | mysql -u khaly -p 
 exec 2> /dev/null
 
 
-mysql test < /home/faye/Bureau/test_2018-02-25.sql.tar.bz2
+mysql baseArestaurer < /home/faye/Bureau/test_2018-02-25.sql.tar.bz2
 echo "Restauration effectué avec succée."
 
 
